e8594bea7f62a3dabb2f25f12024c3bab83e0292
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-20 10:54:57 +0900

    week02 third

--- week02/week02/week02.cpp
+++ week02/week02/week02.cpp
@@ -2,17 +2,21 @@
 
 int main() {
 	int number = 0;
-	int count = 0;
+	//int count = 0;
+	bool isPrime = true; // memory+, readability
 
 	std::cout << "Input number : ";
 	std::cin >> number;
 
 	for (int i = 2; i < number; i++) {
-		if (number % i == 0)
-			count++; // count = count + 1;
+		if (number % i == 0) {
+		//count++; // count = count + 1;
+		isPrime = false; // remove add operation
+		}
 	}
 
-	if (count == 0)
+	//if (count == 0)
+	if (isPrime != 0)
 		std::cout << number << " is prime number~\n";
 	else
 		std::cout << number << " is NOT prime number!\n";
