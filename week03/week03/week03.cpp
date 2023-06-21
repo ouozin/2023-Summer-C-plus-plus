de1e4830647a8116f013e0b964364fe00a69ffd8
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-21 10:48:24 +0900

    week03 pointer v0.2

--- week03/week03/week03.cpp
+++ week03/week03/week03.cpp
@@ -1,17 +1,37 @@
 ﻿#include <iostream>
 using namespace std;
 
-void test(int* pa) { // call by pointer
-	*pa = 7;
-	cout << pa << '\n'; // reference의 주소를 출력함
+void test(const int& ra) {  // call by reference
+	ra = 7; // read only
+	cout << &ra << '\n';
 }
 
 int main() {
 	int a = 1;
-	cout << &a << '\n'; // reference의 주소를 출력함
+	cout << &a << '\n';
 	cout << a << '\n';
-	test(&a);
+	test(a);
 	cout << a << '\n';
+	int a = 9;
+	int b = 3;
+	const int* const pa = &a;
+
+	//*pa = 10;
+	//pa = &b;
+	a = 10;
+
+	cout << *pa << '\n';
+
+	// LLP64 or 4/4/8 (int and long are 32-bit, pointer is 64-bit)
+	cout << sizeof(long int) << '\n';
+	cout << sizeof(int*) << '\n';
+
+	//64 bit systems :
+	//  LLP64 or 4 / 4 / 8 (intand long are 32 - bit, pointer is 64 - bit)
+	//	Win32 API(also called the Windows API) with compilation target 64 - bit ARM(AArch64) or x86 - 64 (a.k.a.x64)
+	//	LP64 or 4 / 8 / 8 (int is 32 - bit, longand pointer are 64 - bit)
+	//	Unix and Unix - like systems(Linux, macOS)
+	// https://en.cppreference.com/w/cpp/language/types
 
 	return 0;
 }
