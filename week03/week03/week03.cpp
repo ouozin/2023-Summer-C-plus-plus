943901fab4de0a57c67879599b057770851822e8
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-21 10:01:09 +0900

    week03 reference v0.3

--- week03/week03/week03.cpp
+++ week03/week03/week03.cpp
@@ -1,12 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 
-void test(int ra) {
+void test(int ra) { // call by reference
 	ra = 7;
+	cout << &ra << '\n'; // reference의 주소를 출력함
 }
 
 int main() {
 	int a = 1;
+	cout << &a << '\n'; // reference의 주소를 출력함
 	cout << a << '\n';
 	test(a);
 	cout << a << '\n';
