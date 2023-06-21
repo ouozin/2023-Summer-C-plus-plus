e81603a83e308482538214fc33074b563db12853
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-21 09:47:47 +0900

    week03 v.01

---
+++ week03/week03/week03.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 1;
+	int& ra = a;
+	int b = 2;
+	//int& ra = b; // Error
+	// int& rc = 99; // Error
+	const int& rc = 99;
+	const int d = 7;
+	//int& rd = d; // Error: 초기화시키지 않고 바로 값을 할당함
+	const int& rd = d;
+
+	cout << a << " " << ra << '\n';
+	cout << rc << '\n';
+	cout << d << " " << rd << '\n';
+
+	return 0;
+}
