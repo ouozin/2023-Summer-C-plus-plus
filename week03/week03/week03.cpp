cd961055516cec1bbc7965d345cc9a35ced86284
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-21 10:30:25 +0900

    week03 pointer v0.1

--- week03/week03/week03.cpp
+++ week03/week03/week03.cpp
@@ -1,16 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 
-void test(int ra) { // call by reference
-	ra = 7;
-	cout << &ra << '\n'; // reference의 주소를 출력함
+void test(int* pa) { // call by pointer
+	*pa = 7;
+	cout << pa << '\n'; // reference의 주소를 출력함
 }
 
 int main() {
 	int a = 1;
 	cout << &a << '\n'; // reference의 주소를 출력함
 	cout << a << '\n';
-	test(a);
+	test(&a);
 	cout << a << '\n';
 
 	return 0;
