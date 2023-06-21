2cfea949e2ffd82976f33c343bcfd054c9e60713
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-21 11:14:46 +0900

    week03 pointer v0.3

--- week03/week03/week03.cpp
+++ week03/week03/week03.cpp
@@ -1,37 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-void test(const int& ra) {  // call by reference
-	ra = 7; // read only
-	cout << &ra << '\n';
-}
-
 int main() {
-	int a = 1;
-	cout << &a << '\n';
-	cout << a << '\n';
-	test(a);
-	cout << a << '\n';
 	int a = 9;
-	int b = 3;
-	const int* const pa = &a;
+	int* pa = &a;
+	int** ppa = &pa;
 
-	//*pa = 10;
-	//pa = &b;
-	a = 10;
+	//ppa = nullptr; // C++11
 
-	cout << *pa << '\n';
+	//ppa = 0;
+	//ppa = NULL;
 
-	// LLP64 or 4/4/8 (int and long are 32-bit, pointer is 64-bit)
-	cout << sizeof(long int) << '\n';
-	cout << sizeof(int*) << '\n';
-
-	//64 bit systems :
-	//  LLP64 or 4 / 4 / 8 (intand long are 32 - bit, pointer is 64 - bit)
-	//	Win32 API(also called the Windows API) with compilation target 64 - bit ARM(AArch64) or x86 - 64 (a.k.a.x64)
-	//	LP64 or 4 / 8 / 8 (int is 32 - bit, longand pointer are 64 - bit)
-	//	Unix and Unix - like systems(Linux, macOS)
-	// https://en.cppreference.com/w/cpp/language/types
+	//ppa = '\0';
+	if (ppa) {
+		cout << a << " " << *pa << " " << **ppa << '\n'; // ** ppa기 가리키는 곳의 가리키는 곳의 값
+		cout << &a << " " << pa << " " << *ppa << '\n';
+	}
 
 	return 0;
 }
