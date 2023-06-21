103d37fe8039b7ed0124f37c6b63f4ac1f92bbe8
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-21 09:54:52 +0900

    week03 reference v0.2

--- week03/week03/week03.cpp
+++ week03/week03/week03.cpp
@@ -1,20 +1,15 @@
 ﻿#include <iostream>
 using namespace std;
 
+void test(int ra) {
+	ra = 7;
+}
+
 int main() {
 	int a = 1;
-	int& ra = a;
-	int b = 2;
-	//int& ra = b; // Error
-	// int& rc = 99; // Error
-	const int& rc = 99;
-	const int d = 7;
-	//int& rd = d; // Error: 초기화시키지 않고 바로 값을 할당함
-	const int& rd = d;
-
-	cout << a << " " << ra << '\n';
-	cout << rc << '\n';
-	cout << d << " " << rd << '\n';
+	cout << a << '\n';
+	test(a);
+	cout << a << '\n';
 
 	return 0;
 }
