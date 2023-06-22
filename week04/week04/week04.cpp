a45ace9a78424d0d5b93ad60abfe536ce7279a49
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-22 09:30:31 +0900

    week04 static memory v0.1

---
+++ week04/week04/week04.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+	int id;
+	string name;
+	static int count;
+public:
+	Student() {
+		cout << "default constructor!\n";
+		count++;
+	}
+	static int getCount() { // inline
+		return count;
+	}
+
+};
+
+int Student::count = 0; // initialize
+
+int main() {
+	Student s1;
+	cout << s1.getCount() << '\n';
+	cout << Student::getCount() << '\n';
+	return 0;
+}
