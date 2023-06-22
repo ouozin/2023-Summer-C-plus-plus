2aef2443f75931a0ee07ffdef946fbc9d35c3542
qkrdbwls <qkrdbwls9999@gmail.com>
2023-06-22 09:41:02 +0900

    week04 static memory v0.2

--- week04/week04/week04.cpp
+++ week04/week04/week04.cpp
@@ -18,11 +18,19 @@ public:
 
 };
 
+void test() {
+	Student s3;
+	cout << Student::getCount() << '\n';
+}
+
+
 int Student::count = 0; // initialize
 
 int main() {
 	Student s1;
 	cout << s1.getCount() << '\n';
 	cout << Student::getCount() << '\n';
+	test();
+	cout << Student::getCount() << '\n'; // 3, ???
 	return 0;
 }
